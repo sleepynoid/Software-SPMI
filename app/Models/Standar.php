4c94704309330d5953811a9ce8c12eb383a327a2
sleepy <aliffarhan@gmail.com>
2024-07-21 10:43:53 +0700

    add migrate and model

---
+++ app/Models/Standar.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Standar extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'id_penetapan',
+        'note',
+        'tipe',
+    ];
+}
