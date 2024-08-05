451bb9349aef0e153bb8cd0fcc68391e188e2c17
aui_tuthousend <mraihan286@gmail.com>
2024-08-05 19:41:26 +0700

    update: routing view

--- app/Http/Controllers/JsonController.php
+++ app/Http/Controllers/JsonController.php
@@ -39,41 +39,6 @@ class JsonController extends Controller
         return response()->json($response);
     }
 
-    public function GetSheet(){
-        $penetapan = Penetapan::find(7);
-        $standars = Standar::where('id_penetapan', $penetapan->id)->where('tipe', '=', 'input')->get();
-        $indikator = Indikator::all();
-        $target = Target::all();
-
-        $response = [];
-        foreach ($standars as $s) {
-
-            $data = [
-                'standar' => $s->note,
-                'indicators' => []
-            ];
-
-
-            foreach ($indikator as $i){
-                if ($i->id_standar == $s->id){
-                    $tar = null;
-                        foreach ($target as $t){
-                        if ($t->id_indikator == $i->id){
-                            $tar = $t;
-                        }
-                    }
-
-                    $newIndicator = ['indicator' => $i->note, 'target' => $tar->value];
-                    array_push($data['indicators'], $newIndicator);
-                }
-            }
-
-            array_push($response, $data);
-        }
-
-
-        return view('welcome', ['name' => $response]);
-    }
 
     public function aa(){
         $penetapan = Penetapan::find(27);
@@ -183,5 +148,19 @@ class JsonController extends Controller
         return response()->json($respond);
     }
 
+    public function getPeriode(String $jurusan){
+//        $shee = Sheet::find($jurusan);
+        $sheets = Sheet::where('jurusan', $jurusan)->get();
+
+
+        return response()->json($sheets);
+//        return response()->json($jurusan);
+    }
+
+    public function getSheet($jurusan, $periode){
+        $sheets = Sheet::where('jurusan', $jurusan)->where('periode', $periode)->where('tipe_sheet', 'pendidikan')->first();
+
+        return $sheets->id;
+    }
 
 }
--- resources/js/components/homepage.vue
+++ resources/js/components/homepage.vue
@@ -1,17 +1,133 @@
 <script setup>
+import {ref, watchEffect} from "vue";
+import { dotStream } from 'ldrs'
+dotStream.register()
+
+const message = ref('upload new sheet')
+const jurusan = ref('a')
+const idS = ref('0')
+const periode = ref('0')
+const per = ref([])
+const loading = ref(true);
+
+
+function notify(){
+    alert('Pilih jurusan 😈')
+}
+
+watchEffect(async ()=> {
+    loading.value = true;
+    let response = await fetch(`/api/getPeriode/${jurusan.value}`);
+    per.value = await response.json();
+    loading.value = false;
+
+    console.log(per)
+})
+
+watchEffect(async ()=> {
+    // loading.value = true;
+    let response = await fetch(`/api/getSheet/${jurusan.value}/${periode.value}`);
+    idS.value = await response.json();
+    // loading.value = false;
+
+    console.log(idS)
+})
+
+
+
 </script>
 
 <template>
-   <router-link :to="{ name: 'Sheet', params: {idSheet: 27}}">Sheet</router-link>
 
-  <router-link to="/import">Import</router-link>
+    <div class="c1">
+        <h1>Mode Super User</h1>
+        <button>
+              <router-link
+                  to="/import"
+                  class="custom-router-link"
+                  :title="message"
+              ><h4>+</h4></router-link>
+        </button>
+    </div>
 
-  <router-link to="/upload">Upload</router-link>
+    <div class="c2">
+        <h5>Pilih Jurusan:</h5>
+        <div>
+            <select v-model="jurusan" class="pilihSheet" required>
+                <option disabled value="">select sheet</option>
+                <option>Teknik Informatika</option>
+                <option>Teknik Elektro</option>
+                <option>Teknik Lingkungan</option>
+                <option>Sistem Informasi</option>
+            </select>
 
-<!--    <router-link to="/testingImport">Testing Import</router-link>-->
+            <button v-if="jurusan == 'a'" @click.prevent="notify"><h5>go</h5></button>
+            <div v-if="loading">
+                <l-dot-stream
+                    size="60"
+                    speed="2.5"
+                    color="black"
+                ></l-dot-stream>
+            </div>
+            <div v-else-if="per.length == 0">
+                <p>Sheet dengan jurusan {{ jurusan }} belum ada</p>
+            </div>
+            <div v-else class="periode">
+                <h5>Pilih Periode:</h5>
+                <select v-model="periode" class="pilihSheet" required>
+                    <option v-for="p in per">{{ p.periode }}</option>
+                </select>
+<!--                {{idS}}-->
+
+                <button>
+                    <router-link
+                        :to="{ name: 'Sheet', params: {idsheet: idS}}"
+                        class="custom-router-link"
+                    ><h5>go</h5></router-link>
+                </button>
+            </div>
+        </div>
+
+    </div>
+
+
+<!--   <router-link :to="{ name: 'Sheet', params: {idSheet: 27}}">Sheet</router-link>-->
+
+
+<!--  <router-link to="/upload">Upload</router-link>-->
 
 </template>
 
 <style scoped>
+
+.c1{
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+}
+
+.c2{
+    //display: flex;
+    margin-top: 3rem;
+    gap: 1rem;
+}
+
+.custom-router-link {
+    text-decoration: none;
+    color: inherit;
+}
+
+.pilihSheet{
+    width: 60%;
+}
+
+.periode{
+    margin-top: 3rem;
+    margin-bottom: 2rem;
+
+    > select{
+        width: 40%;
+    }
+}
 
 </style>
