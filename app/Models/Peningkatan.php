af4e201665728f97c56c00b1803cc0fe477fb3aa
sleepy <aliffarhan@gmail.com>
2024-09-02 11:00:35 +0700

    add peningkatan

---
+++ app/Models/Peningkatan.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Peningkatan extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'komentar'
+    ];
+}
